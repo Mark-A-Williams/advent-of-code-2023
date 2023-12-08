7d2cb6dae096d8d280418740810dcd29ee5d1849
Mark Williams <mark.williams@enable.com>
2023-12-08 22:08:45 +0000

    This could take a while

--- dotnet/Day8.cs
+++ dotnet/Day8.cs
@@ -29,6 +29,48 @@ public class Day8
         return instructionsFollowed;
     }
 
+    public static int ExecutePart2()
+    {
+        var lines = File.ReadAllLines("../inputs/8.txt");
+        var instructions = lines[0];
+
+        var nodes = lines.Skip(2).Select(ParseInputLine)
+            .ToDictionary(node => node.Id, node => node);
+
+        var currentNodes = nodes.Where(n => n.Key[2] == 'A').Select(n => n.Value).ToList();
+        var instructionsFollowed = 0;
+
+        bool areWeThereYet = false;
+        do
+        {
+            var index = instructionsFollowed % instructions.Length;
+            var instruction = instructions[index];
+
+            List<Node> nextNodes = [];
+            foreach (var node in currentNodes)
+            {
+                nextNodes.Add(instruction == 'L' ? nodes[node.LeftNext] : nodes[node.RightNext]);
+            }
+
+            var numberOfZ = nextNodes.Count(n => n.Id[2] == 'Z');
+
+            if (numberOfZ > 3)
+            {
+                Console.WriteLine($"{instructionsFollowed} instructions in, {numberOfZ} simultaneous Zs");
+            }
+
+            areWeThereYet = numberOfZ == nextNodes.Count;
+
+            instructionsFollowed++;
+            currentNodes = nextNodes;
+
+            if (instructionsFollowed % 10_000_000 == 0) Console.WriteLine($"{instructionsFollowed} instructions in");
+        }
+        while (!areWeThereYet);
+
+        return instructionsFollowed;
+    }
+
     private static Node ParseInputLine(string line)
         => new(line.Substring(0, 3), line.Substring(7, 3), line.Substring(12, 3));
 
